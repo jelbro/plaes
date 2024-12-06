31104bdcc98024f92c5d23f0f4a832ab2919897a
Jelbro <Jelbro97@gmail.com>
2024-12-06 16:14:44 +0000

    Add test_ingredient_init

--- tests/test_FoodItems.py
+++ tests/test_FoodItems.py
@@ -1,1 +1,18 @@
+import sys
+
+sys.path.append("/home/josh/documents/development/repos/plaes")
 from FoodItems import Recipe, Ingredient
+
+
+def test_Ingredient_init():
+    apple = Ingredient("Apple", 1)
+
+    assert apple.name == "Apple"
+    assert apple.quantity == 1
+    assert apple.unit == None
+
+    mayo = Ingredient("Mayo", 1, "kg")
+
+    assert mayo.name == "Mayo"
+    assert mayo.quantity == 1
+    assert mayo.unit == "kg"
