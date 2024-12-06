45193ab66eec863bdd053d11f7dcbdb4b986d458
Jelbro <Jelbro97@gmail.com>
2024-12-06 15:59:30 +0000

    Remove test code

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from FoodItems import Recipe, Ingredient
 
 
-def main():
-    chicken = Ingredient("Chicken Breast", 5, "kg")
-    mayonaise = Ingredient("Mayonaise", 1, "kg")
-    lemon = Ingredient("Lemon", 3)
-    white_wine_vinegar = Ingredient("White wine vinegar", 50, "ml")
-
-    chicken_salad = Recipe(
-        "Chicken Salad",
-        [chicken, mayonaise, lemon, white_wine_vinegar],
-        2,
-        4,
-        "4 Litres",
-    )
-
-    print(chicken_salad)
+def main(): ...
 
 
 if __name__ == "__main__":
