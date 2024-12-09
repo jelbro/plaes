e2f053a6fe2ffed5ccc5af9859fc9ac3cea67ff5
Jelbro <Jelbro97@gmail.com>
2024-12-09 11:59:31 +0000

    Add test to remove from ingredient for removing past zero

--- tests/test_FoodItems.py
+++ tests/test_FoodItems.py
@@ -110,3 +110,16 @@ def test_Recipe_remove_from_ingredient_removing_to_zero():
 
     with pytest.raises(Exception) as e_info:
         toast.remove_from_ingredient("Butter", 10)
+
+
+def test_Recipe_remove_from_ingredient_removing_past_zero():
+    toast = Recipe(
+        "Toast",
+        (Ingredient("Bread", 1, "slice"), Ingredient("Butter", 10, "g")),
+        0,
+        1,
+        "slice",
+    )
+
+    with pytest.raises(Exception) as e_info:
+        toast.remove_from_ingredient("Butter", 20)
