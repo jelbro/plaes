9f70e484ad1e8e3ab8bd3ee78745089c0cd3eb48
Josh Elbro <jelbro97@gmail.com>
2024-12-19 16:13:45 +0000

    Update main code

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
 from FoodItems import Recipe, Ingredient, load_recipe, load_ingredients
 from Menu import *
-import json
 
 
 def main():
-    """
-    chicken_salad = Recipe(
-        name="Chicken Salad",
-        quantity=0,
-        desired_quantity=1,
-        unit="4litre",
-    )
-
-    chicken_salad.add_ingredient(Ingredient("Chicken Breast", 5, "kg"))
-    chicken_salad.add_ingredient(Ingredient("Mayo", 1, "kg"))
-    chicken_salad.add_ingredient(Ingredient("Lemon", 3))
-    chicken_salad.add_ingredient(Ingredient("White Wine Vinegar", 50, "ml"))
-    print(chicken_salad)
-
-
-    chicken_salad.save_recipe(
-        "test.json",
-    )
-    chicken_salad = load_recipe("test.json")
-    print(chicken_salad)
-    """
     instance = Menu()
     instance.display_main_menu()
 
