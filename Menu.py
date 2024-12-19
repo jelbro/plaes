a5876edce788ee7d819203573d3f6e0328921d24
Josh Elbro <jelbro97@gmail.com>
2024-12-19 14:50:09 +0000

    Add menu draft code

--- Menu.py
+++ Menu.py
@@ -5,7 +5,7 @@ class Menu:
             "Select an option",
             "i: Ingredients r: Recipes p: Prep List"
             )
-        choice = self.get_menu_choice(['i', 'r', 'p'])
+        choice = self.get_menu_choice(['i', 'r', 'p', 'q'])
         match choice:
             case 'i':
                 self.display_ingredient_menu()
@@ -13,11 +13,29 @@ class Menu:
                 self.display_recipe_menu()
             case 'p':
                 self.display_prep_list_menu()
+            case 'q':
+                self.display_quit_menu()
 
     
     def display_ingredient_menu(self):
-        ...
-
+        print(
+            "   Ingredient Menu    ",
+            "Select an option",
+            "v: View Ingredients a: Add Ingredient d: Delete Ingredient"
+        )
+        choice = self.get_menu_choice(['v', 'a', 'd', 'b'])
+        match choice:
+            case 'v':
+                ...
+                #IngredientsList.display_list()
+            case 'a':
+                ...
+                #IngredientsList.add_to_list()
+            case 'd':
+                ...
+                #IngredientsList.delete_from_list()
+            case 'b':
+                self.display_main_menu()
     
     def display_recipe_menu(self):
         ...
@@ -26,6 +44,9 @@ class Menu:
     def display_prep_list_menu(self):
         ...
 
+
+    def dsiplay_quit_menu(self):
+        ...
 
     def get_menu_choice(self, options):
         while True:
