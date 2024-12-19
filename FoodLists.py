9499a3a15ce9b17daa2c55576ad16d05769d1ee9
Josh Elbro <jelbro97@gmail.com>
2024-12-19 17:39:16 +0000

    Add initial code

---
+++ FoodLists.py
@@ -0,0 +1,17 @@
+from FoodItems import *
+
+
+class IngredientList:
+    def __init__(self, ingredient_list=[]):
+        self.ingredient_list = []
+        for ingredient in ingredient_list:
+            self.ingredient_list.append(ingredient)
+
+    def add_to_list(self, ingredient):
+        self.ingredient_list.append(ingredient)
+
+    def display_list(self):
+        num = 1
+        for ingredient in self.ingredient_list:
+            print(f"{num}:{ingredient.name}, unit: {ingredient.unit}")
+            num += 1
