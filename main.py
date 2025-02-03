00f79597a7e0b5f16094491fc0668e419f28c438
Jelbro <Jelbro97@gmail.com>
2025-02-03 18:03:17 +0000

    Add ability to quit without saving

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def main():
     gui = Gui(root, ingredient_list=ingredient_list, recipe_list=recipe_list)
 
     def on_closing():
-        if messagebox.askokcancel(
-            "Quit", "Do you want to save before quitting?"
-        ):
+        if messagebox.askyesno("Quit", "Do you want to save before quitting?"):
             save_lists(
                 [gui.ingredient_list, gui.recipe_list], "plaes_lists.json"
             )
+            root.destroy()
+        else:
             root.destroy()
 
     root.protocol("WM_DELETE_WINDOW", on_closing)
